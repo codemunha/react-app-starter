d04dbff5abf69670b3eae44d08a4dfa05c93c355
Bird <bird@BIRD-MBPs-MacBook-Pro.local>
2019-11-06 00:04:44 +0700

    init config react

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// it('renders without crashing', () => {
+//   const div = document.createElement('div')
+//   ReactDOM.render(<App />, div)
+//   ReactDOM.unmountComponentAtNode(div)
+// })
+import React from "react"
+import { render } from "@testing-library/react"
+import App from "./App"
+
+it("renders without crashing", () => {
+  const { getByText } = render(<App />)
+  expect(getByText("Learn React")).toBeInTheDocument()
+})
